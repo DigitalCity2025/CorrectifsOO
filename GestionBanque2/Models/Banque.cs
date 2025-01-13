05cec7936f7dd76aa08cb5c12d6a7c176bf8f75e
Khun <khun.ly@bstorm.be>
2025-01-13 09:58:38 +0100

    indexeurs

---
+++ GestionBanque2/Models/Banque.cs
@@ -0,0 +1,44 @@
+﻿using GezstionBanque1.Models;
+
+
+namespace GestionBanque2.Models
+{
+    public class Banque
+    {
+        private List<Courant> courants = new ();
+        public string Nom { get; set; }
+
+        public Courant? this[string numero]
+        {
+            get 
+            {
+                Courant? c = courants.Find(c => c.Numero == numero);
+                return c;
+            }
+        } 
+
+        public void Ajouter(Courant courant)
+        {
+            // permet de vérifier si il existe déjà un compte dans la banque avec ce numero 
+            if(courants.Any(c => c.Numero == courant.Numero))
+            {
+                // plus déclencher une erreur
+                return;
+            } 
+            courants.Add(courant);
+        }
+
+        public void Supprimer(string numero)
+        {
+            // Chercher un compte dans la liste de comptes
+            // qui repond à une condition
+            Courant? c = courants.Find(c => c.Numero == numero);
+            if(c == null)
+            {
+                // plus déclencher une erreur
+                return;
+            }
+            courants.Remove(c);
+        } 
+    }
+}
--- GestionBanque2/Program.cs
+++ GestionBanque2/Program.cs
@@ -1,1 +1,87 @@
-﻿
+﻿using GestionBanque2.Models;
+using GezstionBanque1.Models;
+
+Banque banque = new()
+{
+    Nom = "Banque de Digital City"
+};
+
+ConsoleKey key = default;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("1. Ajouter un compte");
+    Console.WriteLine("2. Afficher un compte");
+    key = Console.ReadKey(true).Key;
+    switch (key)
+    {
+        case ConsoleKey.NumPad1:
+            AjouterCompte();
+            break;
+        case ConsoleKey.NumPad2:
+            AfficherCompte();
+            break;
+    }
+}
+
+void AfficherCompte()
+{
+    string numero = Question("Entrer le numéro");
+    Courant? c = banque[numero];
+    if (c == null)
+    {
+        Console.WriteLine("Aucun compte existant avec ce numero");
+        return;
+    }
+    Console.WriteLine($"{c.Numero} {c.Solde} {c.Titulaire.Nom} {c.Titulaire.Prenom}");
+    Console.WriteLine("1. Ajouter de l'argent");
+    Console.WriteLine("2. Retirer de l'argent");
+
+    key = Console.ReadKey(true).Key;
+    switch (key)
+    {
+        case ConsoleKey.NumPad1:
+            AjouterArgent(c);
+            break;
+        case ConsoleKey.NumPad2:
+            RetirerArgent(c);
+            break;
+    }
+}
+
+void AjouterArgent(Courant c)
+{
+    double montant = double.Parse(Question("Quel montant ?"));
+    c.Depot(montant);
+}
+
+void RetirerArgent(Courant c)
+{
+    double montant = double.Parse(Question("Quel montant ?"));
+    c.Retrait(montant);
+}
+
+void AjouterCompte()
+{
+    Personne p = new()
+    {
+        Nom = Question("Entrer un nom"),
+        Prenom = Question("Entrer un prenom"),
+        DateNaissance = DateTime.Parse(Question("Entrer une date de naissance")),
+    };
+
+    Courant c = new()
+    {
+        Numero = Question("Entrer le numero"),
+        Titulaire = p,
+        LigneDeCredit = double.Parse(Question("Entrer la ligne de crédit"))
+    };
+    banque.Ajouter(c);
+}
+
+string Question(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
